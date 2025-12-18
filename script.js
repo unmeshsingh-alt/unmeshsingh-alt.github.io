61a23a40ddd6582c1f8500a1791d735966ae4cb3
Unmesh Singh <unmesh.singh@niser.ac.in>
2025-12-18 06:31:30 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -5,4 +6,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       behavior: 'smooth'
     });
   });
+});
+
+// Header interaction on scroll
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
